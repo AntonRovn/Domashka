4177669a8b366f8735bd186f4a733fdf9e963b85
Anton <102684291+AntonRovn@users.noreply.github.com>
2022-04-06 11:27:05 +0300

    Add files via upload

---
+++ HomeWorkEasy/HomeWork/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    //Program to show logical operations, primitive data types
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Please Enter your age");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter your gender, Type M for Man,  W for Woman");
+        char gender = Console.ReadLine()[0];
+        if ((gender == 'M') || (gender == 'W'))
+        {
+            if (gender == ('W'))
+            {
+                string result = (age < 40) ? "Hello Miss. " : "Hello Mrs.";
+                Console.WriteLine(result + name);
+            }
+            else
+                Console.WriteLine("Hello Mister " + name);
+        }
+        else
+        {
+            Console.WriteLine("Error. Type M for Man,  W for Woman");
+
+
+
+
+        }
+    }
+}
